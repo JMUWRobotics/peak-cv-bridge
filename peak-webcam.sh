cd8f9ad7c81a5893669dbf09c6f5319013d98242
Sir-Photch <christoph.liebender@posteo.de>
2025-02-20 09:57:22 +0100

    fix script

--- peak-webcam.sh
+++ peak-webcam.sh
@@ -3,5 +3,5 @@
 DEVICE_PATH="/dev/video42"
 DEVICE_NAME="Peak Webcam"
 
-test -e $DEVICE_PATH || sudo v4l2loopback-ctl add --buffers 2 --name $DEVICE_NAME $DEVICE_PATH
+test -e $DEVICE_PATH || sudo v4l2loopback-ctl add --buffers 2 --name "$DEVICE_NAME" $DEVICE_PATH
 peakcvbridge-capture --v4l2loopback=$DEVICE_PATH --auto-exposure --framerate 30.0
